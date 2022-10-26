5c0ad95ecf48c3a1cdb8db0009e774786c76d3b2
Matt Fisher <matt.fisher@nsidc.org>
2022-10-26 17:44:00 -0600

    Add black/isort formatting tooling

--- sipn_reanalysis_ingest/util/process_cfsr.py
+++ sipn_reanalysis_ingest/util/process_cfsr.py
@@ -1,33 +1,47 @@
 ## function to process CFSR data
 ## TODO
-#	- from NCAR best?
-#	- How often new data available
+# 	- from NCAR best?
+# 	- How often new data available
 ## ENDToDo
 
 import glob
 import tarfile
+
 import Nio
 
 
 def process_cfsr():
-   vars=["TMP_P0_L103_GGA0","UGRD_P0_L103_GGA0","VGRD_P0_L103_GGA0","PWAT_P0_L200_GLL0","SPFH_P0_L103_GLL0","RH_P0_L103_GLL0","UGRD_P0_L100_GLL0","VGRD_P0_L100_GLL0","SPFH_P0_L100_GLL0"]
-   untar_files()
-   read_grib(vars)
+    vars = [
+        "TMP_P0_L103_GGA0",
+        "UGRD_P0_L103_GGA0",
+        "VGRD_P0_L103_GGA0",
+        "PWAT_P0_L200_GLL0",
+        "SPFH_P0_L103_GLL0",
+        "RH_P0_L103_GLL0",
+        "UGRD_P0_L100_GLL0",
+        "VGRD_P0_L100_GLL0",
+        "SPFH_P0_L100_GLL0",
+    ]
+    untar_files()
+    read_grib(vars)
+
 
 def untar_files():
-   datadir="/data/ecassano/Current_projects/SIPN2/CFSR/"
-   numfiles=glob.glob(datadir+"*.1979*tar")
-   for i in numfiles:
-      tar=tarfile.open(i)
-      tar.extractall()
-      tar.close()
+    datadir = "/data/ecassano/Current_projects/SIPN2/CFSR/"
+    numfiles = glob.glob(datadir + "*.1979*tar")
+    for i in numfiles:
+        tar = tarfile.open(i)
+        tar.extractall()
+        tar.close()
+
 
 def read_grib(vars):
-   numfiles=glob.glob("*19790101*.grb2")
-   for i in numfiles:
-      fn=Nio.open_file(i)
-      for v in vars:
-         var=fn.variables[v]
-      
+    numfiles = glob.glob("*19790101*.grb2")
+    for i in numfiles:
+        fn = Nio.open_file(i)
+        for v in vars:
+            var = fn.variables[v]
+
+
 if __name__ == "__main__":
-   process_cfsr()
+    process_cfsr()
--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,6 +1,8 @@
 from invoke import Collection
 
 from . import env
+from . import format as format_
 
 ns = Collection()
 ns.add_collection(env)
+ns.add_collection(format_)
