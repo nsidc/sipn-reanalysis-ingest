86eed0ba0290c6c7f4311d254d044cef9cb55590
Matt Fisher <matt.fisher@nsidc.org>
2022-11-08 11:05:28 -0700

    Add testing CLI to convert module

--- sipn_reanalysis_ingest/util/convert.py
+++ sipn_reanalysis_ingest/util/convert.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import click
+
 from sipn_reanalysis_ingest.util.log import logger
 
 
@@ -13,3 +15,27 @@ def convert_grib2s_to_nc(
 
     logger.info(f'Created {output_path}')
     return output_path
+
+
+if __name__ == '__main__':
+    @click.command()
+    @click.argument(
+        'grib2_files',
+        nargs=-1,
+    )
+    @click.option(
+        '-o', '--output', 'output_path',
+        type=click.Path(),
+        help='The path the output .nc file will be written to',
+        required=True,
+    )
+    def cli_test_convert(grib2_files, output_path):
+        """Test this module from CLI.
+
+        e.g.:
+
+            PYTHONPATH=. python sipn_reanalysis_ingest/util/convert.py
+        """
+        convert_grib2s_to_nc(grib2_files, output_path=output_path)
+
+    cli_test_convert()
