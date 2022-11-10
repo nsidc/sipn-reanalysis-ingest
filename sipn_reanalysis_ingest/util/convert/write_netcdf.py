bbced011125c7b27df347980bc9d5ba8a8c3ff24
Matt Fisher <matt.fisher@nsidc.org>
2022-11-10 14:33:24 -0700

    Add missing paren

--- sipn_reanalysis_ingest/util/convert/write_netcdf.py
+++ sipn_reanalysis_ingest/util/convert/write_netcdf.py
@@ -1,12 +1,13 @@
+import Ngl
+import Nio
 import numpy as np
-import Ngl, Nio
 
 
 def write_netcdf(array):
    outf=Nio.open_file("cfsr."+date+".nc","c")
 
-   outf.create_dimension('lat_0',array.dimensions['lat_0'] 
+   outf.create_dimension('lat_0',array.dimensions['lat_0'])
 
-   outf=create_variable('lat_0',lat_0.typecode(),lat.dimensions)
+   outf.create_variable('lat_0',lat_0.typecode(),lat.dimensions)
 
    outf.create_variable('t','f',('time','lat','lon')
