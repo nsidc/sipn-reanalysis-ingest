0c98c07b02698830d50847cdd8b2e07c71e832f0
Matt Fisher <matt.fisher@nsidc.org>
2022-11-09 18:23:41 -0700

    Update convert CLI to accept analysis and forecast inputs separately

--- sipn_reanalysis_ingest/util/convert.py
+++ sipn_reanalysis_ingest/util/convert.py
@@ -11,6 +11,15 @@ def convert_grib2s_to_nc(
     forecast_inputs: list[Path],
     output_path: Path,
 ) -> Path:
+    if not (
+        len(analysis_inputs) == 4
+        and len(forecast_inputs) == 4
+    ):
+        raise CfsrInputDataError(
+            'Expected 4 of each type of file. Received:'
+            f' {analysis_inputs=}; {forecast_inputs=}'
+        )
+
     with open(output_path, 'w') as f:
         f.write('NetCDF data goes in here!\n')
         f.write('\n')
@@ -30,9 +39,21 @@ def convert_grib2s_to_nc(
 if __name__ == '__main__':
 
     @click.command()
-    @click.argument(
-        'grib2_files',
-        nargs=-1,
+    @click.option(
+        '-a',
+        '--analysis-inputs',
+        type=click.Path(),
+        nargs=4,
+        help='Exactly four 6-hourly CFSR analysis inputs.',
+        required=True,
+    )
+    @click.option(
+        '-f',
+        '--forecast-inputs',
+        type=click.Path(),
+        nargs=4,
+        help='Exactly four 6-hourly CFSR forecast inputs.',
+        required=True,
     )
     @click.option(
         '-o',
@@ -42,13 +63,17 @@ if __name__ == '__main__':
         help='The path the output .nc file will be written to',
         required=True,
     )
-    def cli_test_convert(grib2_files, output_path):
+    def cli_test_convert(analysis_inputs, forecast_inputs, output_path):
         """Test this module from CLI.
 
         e.g.:
 
             PYTHONPATH=. python sipn_reanalysis_ingest/util/convert.py
         """
-        convert_grib2s_to_nc(grib2_files, output_path=output_path)
+        convert_grib2s_to_nc(
+            analysis_inputs=analysis_inputs,
+            forecast_inputs=forecast_inputs,
+            output_path=output_path,
+        )
 
     cli_test_convert()
