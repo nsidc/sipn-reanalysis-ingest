4759cd4f240609a91b03676735a9c99cdb022398
Matt Fisher <matt.fisher@nsidc.org>
2022-12-20 23:51:03 +0000

    Format with `black`

--- sipn_reanalysis_ingest/util/convert/read_grib_daily.py
+++ sipn_reanalysis_ingest/util/convert/read_grib_daily.py
@@ -12,68 +12,72 @@ Will return an array of daily data for eventual output to a single netcdf file
 
 from pathlib import Path
 
-import xarray as xr
 import Nio
 import numpy as np
 import rioxarray
+import xarray as xr
 
 import sipn_reanalysis_ingest.constants.variables_daily as variables
 from sipn_reanalysis_ingest.util.convert.reorg_xarr_daily import (
     reorg_xarr_daily as reorg_xarr,
 )
 
-def read_grib_daily(afiles,ffiles,output_path: Path):
+
+def read_grib_daily(afiles, ffiles, output_path: Path):
 
     # Parse through variables to extract variable names
-    vs=[v for v in dir(variables) if not v.startswith('__')]
-    si=[]
+    vs = [v for v in dir(variables) if not v.startswith('__')]
+    si = []
     for z in vs:
-       si.append(list(getattr(variables,z).values()))
+        si.append(list(getattr(variables, z).values()))
 
-    vari=[]
+    vari = []
     for i in si:
-       [vari.append(x) for x in i if x not in vari]
+        [vari.append(x) for x in i if x not in vari]
 
     # Forecast files
-    fnf = xr.open_mfdataset(ffiles,
-        concat_dim = 't', 
-        combine = 'nested',
-        parallel = True, 
-        engine='pynio')
+    fnf = xr.open_mfdataset(
+        ffiles,
+        concat_dim='t',
+        combine='nested',
+        parallel=True,
+        engine='pynio',
+    )
 
     # Analysis files
-    fna = xr.open_mfdataset(afiles,
-        concat_dim = 't', 
-        combine = 'nested',
-        parallel = True, 
-        engine='pynio')
+    fna = xr.open_mfdataset(
+        afiles,
+        concat_dim='t',
+        combine='nested',
+        parallel=True,
+        engine='pynio',
+    )
 
     # Merge everything into a single dataset (forecast and analysis have unique variable
     # names)
-    fn=fna.merge(fnf,compat='override')
+    fn = fna.merge(fnf, compat='override')
 
     # Remove variables that we do not need
-    totvar=[i for i in fn]
-    rmvars=[x for x in totvar if x not in vari]
-    fnsm=fn.drop_vars(rmvars)
+    totvar = [i for i in fn]
+    rmvars = [x for x in totvar if x not in vari]
+    fnsm = fn.drop_vars(rmvars)
 
     # Extract data to 40N and only grab levels at 925, 850, and 500mb.
-    fnsm=fnsm.isel(lat_0=slice(0,101,1),lv_ISBL0=[21,30,33])
+    fnsm = fnsm.isel(lat_0=slice(0, 101, 1), lv_ISBL0=[21, 30, 33])
 
     # Calculate daily mean
-    newfn=fnsm.mean(dim='t',keep_attrs=True)
+    newfn = fnsm.mean(dim='t', keep_attrs=True)
 
     # Reproject to northern hemisphere polar stereographic
-    newfn.rio.write_crs("epsg:4326",inplace=True)
-    newfn.rio.set_spatial_dims(
-       x_dim="lon_0",
-       y_dim="lat_0",
-       inplace=True)
+    newfn.rio.write_crs("epsg:4326", inplace=True)
+    newfn.rio.set_spatial_dims(x_dim="lon_0", y_dim="lat_0", inplace=True)
     newfn.rio.write_coordinate_system(inplace=True)
-    dataproj=newfn.rio.reproject("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m")
+    dataproj = newfn.rio.reproject(
+        "+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m"
+    )
 
     # Call function to restructure dataset with proper variable names, array sizes, etc.
-    dataout=reorg_xarr(dataproj)
+    dataout = reorg_xarr(dataproj)
 
     # Write newly restructured dataset to a netcdf file
     comp = {"zlib": True, "complevel": 9}
--- sipn_reanalysis_ingest/util/convert/read_grib_monthly.py
+++ sipn_reanalysis_ingest/util/convert/read_grib_monthly.py
@@ -11,53 +11,54 @@ Will return an array of daily data for eventual output to a single netcdf file
 """
 from pathlib import Path
 
-import xarray as xr
 import Nio
 import numpy as np
 import rioxarray
+import xarray as xr
+
 import sipn_reanalysis_ingest.constants.variables_monthly as variables
 from sipn_reanalysis_ingest.util.convert.reorg_xarr_monthly import (
     reorg_xarr_monthly as reorg_xarr,
 )
 
-def read_grib_monthly(afile,ffile,output_path: Path):
 
-# Parse through variables to extract variable names
-    vs=[v for v in dir(variables) if not v.startswith('__')]
-    si=[]
+def read_grib_monthly(afile, ffile, output_path: Path):
+
+    # Parse through variables to extract variable names
+    vs = [v for v in dir(variables) if not v.startswith('__')]
+    si = []
     for z in vs:
-       si.append(list(getattr(variables,z).values()))
+        si.append(list(getattr(variables, z).values()))
 
-    vari=[]
+    vari = []
     for i in si:
-       [vari.append(x) for x in i if x not in vari]
+        [vari.append(x) for x in i if x not in vari]
 
     # Open analysis and forecast monthly file
-    fnf = xr.open_dataset(ffile,engine='pynio')
-    fna = xr.open_dataset(afile,engine='pynio')
+    fnf = xr.open_dataset(ffile, engine='pynio')
+    fna = xr.open_dataset(afile, engine='pynio')
 
     # Merge these into a single dataset
-    fn=fna.merge(fnf,compat='override')
+    fn = fna.merge(fnf, compat='override')
 
     # Remove variables that we do not need
-    totvar=[i for i in fn]
-    rmvars=[x for x in totvar if x not in vari]
-    fnsm=fn.drop_vars(rmvars)
+    totvar = [i for i in fn]
+    rmvars = [x for x in totvar if x not in vari]
+    fnsm = fn.drop_vars(rmvars)
 
     # Extract data to 40N and only grab levels at 925, 850, and 500mb.
-    fnsm=fnsm.isel(lat_0=slice(0,101,1),lv_ISBL0=[21,30,33])
+    fnsm = fnsm.isel(lat_0=slice(0, 101, 1), lv_ISBL0=[21, 30, 33])
 
     # Reproject to northern hemisphere polar stereographic
-    fnsm.rio.write_crs("epsg:4326",inplace=True)
-    fnsm.rio.set_spatial_dims(
-       x_dim="lon_0",
-       y_dim="lat_0",
-       inplace=True)
+    fnsm.rio.write_crs("epsg:4326", inplace=True)
+    fnsm.rio.set_spatial_dims(x_dim="lon_0", y_dim="lat_0", inplace=True)
     fnsm.rio.write_coordinate_system(inplace=True)
-    dataproj=fnsm.rio.reproject("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m")
+    dataproj = fnsm.rio.reproject(
+        "+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m"
+    )
 
     # Call function to restructure dataset with proper variable names, array sizes, etc.
-    dataout=reorg_xarr(dataproj)
+    dataout = reorg_xarr(dataproj)
 
     # Write newly restructured dataset to a netcdf file
     comp = {"zlib": True, "complevel": 9}
