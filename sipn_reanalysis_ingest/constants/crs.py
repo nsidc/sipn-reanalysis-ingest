073e500be844ece2dbc34d3cf8a242897fd812cd
Matt Fisher <matt.fisher@nsidc.org>
2022-12-21 17:35:02 +0000

    Fix bug in CRS string

--- sipn_reanalysis_ingest/constants/crs.py
+++ sipn_reanalysis_ingest/constants/crs.py
@@ -1,5 +1,5 @@
 PROJ_SRC = 'EPSG:4326'
 PROJ_DEST = (
     "+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45"
-    "+k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m"
+    " +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m"
 )
